84a40d402aa6a6d98ce738243c3f42eccca58908
Kononenko Rustam <65700565+DigitalRingo@users.noreply.github.com>
2024-05-02 22:41:35 +0700

    Update chat_bot_shooter.py

--- Модуль 4/Занятие 6/chat_bot_shooter.py
+++ Модуль 4/Занятие 6/chat_bot_shooter.py
@@ -3,7 +3,7 @@ import telebot
 from telebot import types
 
 # В это место нужно вставить токен своего бота!
-API_TOKEN = "6484688443:AAFINyi5BJmVb6JA4yOZPcS5h4IjXh9a1AU"
+API_TOKEN = ""
 bot = telebot.TeleBot(API_TOKEN)
 
 # список активных игроков
